b9204a1bc0f04d30ea87a49ee9a6e9f05c82805f
tiborbozik <160650542+tiborbozik@users.noreply.github.com>
2024-09-06 22:06:10 -0400

    MySong

---
+++ wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+const myvideoSpinning = [
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(360deg) scale(0)" },
+];
+
+const myvideoTiming = {
+    duration: 200,
+    iterations: 1,
+};
+
+const songvideo = document.querySelector(".myvideo");
+
+mybutton.addEventListener("click", () => {
+    songvideo.animate(myvideoSpinning, myvideoTiming);
+});
